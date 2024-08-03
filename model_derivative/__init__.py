a6beb0ecdc8612fb55e873748cf86a7867a92e5b
Armando Aguilar L <a.aguilar@gategeek.com>
2024-08-03 13:41:44 -0700

    update install in readme file

---
+++ model_derivative/__init__.py
@@ -0,0 +1,14 @@
+# model_derivative/__init__.py
+
+"""
+Model Derivative Package
+------------------------
+This package provides functionality to convert Revit file formats and extract 3D model metadata.
+"""
+
+__version__ = "1.0.0"
+
+from .auth import AuthClass
+from .buckets_objects_oss import BucketsObjectsOSS
+from .buckets_oss import BucketsOSS
+from .meta_data import MetaData
